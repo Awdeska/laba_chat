9aad5fe47ace2e4e782fa2629f4c6b5397d3d3aa
Awdeska <hsasha.grodno@gmail.com>
2023-02-25 21:09:06 +0300

    client

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './components/Header/Header';
+import LogInForm from './components/LogInForm/LogInForm';
+
+
+function App() {
+  return (
+    <div className='body'>
+      <Header/>
+      <LogInForm/>
+    </div>
+  );
+}
+
+export default App;
