e90f031b764f99052d0d1b59687434e9f868380f
Awdeska <hsasha.grodno@gmail.com>
2023-02-25 21:05:48 +0300

    server controllers

---
+++ server/controllers/user-controller.js
@@ -0,0 +1,46 @@
+const userService = require('../service/user-service');
+
+class UserController {
+    async registration(req, res, next) {
+        try {
+            const {nickname, password} = req.body;
+            const userData = await userService.registration(nickname, password);
+            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true});
+            return res.json(userData);
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    async login(req, res, next) {
+        try {
+            const {nickname, password} = req.body;
+            const userData = await userService.login(nickname, password);
+            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true});
+            return res.json(userData);
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    async logout(req, res, next) {
+        try {
+            
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    async refresh(req, res, next) {
+        try {
+            const {refreshToken} = req.cookies;
+            const userData = await userService.refresh(refreshToken);
+            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true});
+            return res.json(userData);
+        } catch (error) {
+            next(error);
+        }
+    }
+}
+
+module.exports = new UserController();
