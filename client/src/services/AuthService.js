e7f0ec52b1afdea70f11238521f9c989c98f0d24
Awdeska <hsasha.grodno@gmail.com>
2023-02-25 21:08:08 +0300

    client services

---
+++ client/src/services/AuthService.js
@@ -0,0 +1,23 @@
+import $api from "../http";
+
+export default class AuthService {
+    static async login(nickname, password){
+        return $api.post('/login', {nickname, password}).then((response) => {
+            const items = response.data;
+            console.log(response.data)
+            return items;
+        })
+        .catch((err) => console.log(err));;
+    }
+
+    static async registration(nickname, password) {
+        return $api.post('/registration', {nickname, password}).then((response) => {
+            console.log(response)
+            const items = response.data;
+            return items;
+        })
+        .catch((err) => console.log(err));;
+    }
+
+}
+
