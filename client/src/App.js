74b768c07ba1e3fd6feb76a0955ccb83c05731dc
Awdeska <hsasha.grodno@gmail.com>
2023-02-28 11:21:04 +0300

    add loading

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,27 @@
 import './App.css';
 import Header from './components/Header/Header';
 import LogInForm from './components/LogInForm/LogInForm';
+import {useEffect} from "react";
+import {store} from "./index";
 
 
 function App() {
-  return (
-    <div className='body'>
-      <Header/>
-      <LogInForm/>
-    </div>
-  );
+    useEffect(() => {
+        if (localStorage.getItem('token')) {
+            store.checkAuth()
+        }
+    }, [])
+
+    if (store.isLoading) {
+        return <div>Загрузка...</div>
+    }
+
+    return (
+        <div className='body'>
+            <Header/>
+            <LogInForm/>
+        </div>
+    );
 }
 
 export default App;
