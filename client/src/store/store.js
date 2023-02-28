ccb01fee1adc24ea07be719c2df1ecc97ad73167
Awdeska <hsasha.grodno@gmail.com>
2023-02-28 09:47:50 +0300

    add refresh token

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,14 +1,21 @@
 import {makeAutoObservable} from "mobx";
 import AuthService from "../services/AuthService";
+import {API_URL} from "../http";
+import axios from "axios";
 
 export default class Store {
     user;
+    isLoading;
     constructor() {
         makeAutoObservable(this);
     }
 
     setUser(user) {
         this.user = user;
+    }
+
+    setLoading(bool) {
+        this.isLoading = bool;
     }
 
     async login(nickname, password) {
@@ -28,6 +35,20 @@ export default class Store {
             this.setUser(response.data.user);
         } catch (e) {
             console.log(e.response?.data?.message);
+        }
+    }
+
+    async checkAuth() {
+        this.setLoading(true);
+        try {
+            const response = await axios.get(`${API_URL}/refresh`, {withCredentials: true})
+            console.log(response);
+            localStorage.setItem('token', response.data.accessToken);
+            this.setUser(response.data.user);
+        } catch (e) {
+            console.log(e.response?.data?.message);
+        } finally {
+            this.setLoading(false);
         }
     }
 }
